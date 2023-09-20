03920e063856f696975ad32e33d16414d4cdfa9b
Jeonghyeon Kim <jeonghyeon.kim@cp.kaist.ac.kr>
2023-09-20 08:28:58 +0000

    Remove weak variants temporarily

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,6 @@
 #![feature(associated_type_bounds)]
 mod internal;
 mod strongs;
-mod weaks;
 
 pub use internal::*;
 pub use strongs::*;
-pub use weaks::*;
--- src/strongs.rs
+++ src/strongs.rs
@@ -7,7 +7,7 @@ use std::{
 use atomic::{Atomic, Ordering};
 use static_assertions::const_assert;
 
-use crate::{Acquired, AtomicWeak, Cs, Pointer, Tagged, TaggedCnt, Weak};
+use crate::{Acquired, Cs, Pointer, Tagged, TaggedCnt};
 
 /// A result of unsuccessful `compare_exchange`.
 ///
@@ -379,27 +379,8 @@ impl<T, C: Cs> Snapshot<T, C> {
     }
 
     #[inline]
-    pub fn load_from_weak(&mut self, from: &AtomicWeak<T, C>, cs: &C) -> bool {
-        // TODO: Referencing AtomicWeak from strong snapshot is awkward... Find a better
-        // project/API structure.
-        cs.protect_snapshot(&from.link, &mut self.acquired)
-    }
-
-    #[inline]
     pub fn protect(&mut self, ptr: &Rc<T, C>, cs: &C) {
         cs.reserve(ptr.as_ptr(), &mut self.acquired);
-    }
-
-    #[inline]
-    pub fn protect_weak(&mut self, ptr: &Weak<T, C>, cs: &C) -> bool {
-        cs.reserve(ptr.as_ptr(), &mut self.acquired);
-        if !self.acquired.is_null() {
-            if unsafe { self.acquired.as_ptr().deref() }.ref_count() == 0 {
-                self.acquired.clear();
-                return false;
-            }
-        }
-        true
     }
 
     #[inline(always)]
